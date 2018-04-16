a36b20b3a2467f03ae7a1825e0abf4391b7c3fb4
root <root@zabbix.d2d.com.br>
2018-04-16 15:35:18 -0300

    ok

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ docker build -f build-zabbix/Dockerfile -t registry.d2d.com.br:5000/zabbix:1.0.0
 docker volume create --name=mysql
 docker volume create --name=zabbix
 ```
-
+```bash
 docker-compose up -d
+```
+
+```bash
 docker exec -it mariadb ./script.sh
+```
+
--- build-zabbix/run.sh
+++ build-zabbix/run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-
 sleep 30
 echo "USE zabbix" |  mysql -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
 
 if [ $? -ne 0 ]; then
+
 	cd /usr/share/doc/zabbix-server-mysql/
 	echo "CREATE DATABASE zabbix" | mysql -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
-	zcat create.sql.gz | mysql zabbix -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS 
+	zcat create.sql.gz | mysql zabbix -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
+	
+	sed -i 's/$ZB_MYSQL_HOST/'$ZB_MYSQL_HOST'/g'    /etc/zabbix/zabbix_server.conf
+	sed -i 's/$ZB_MYSQL_BD/'$ZB_MYSQL_BD'/g'        /etc/zabbix/zabbix_server.conf
+	sed -i 's/$ZB_MYSQL_USER/'$ZB_MYSQL_USER'/g'    /etc/zabbix/zabbix_server.conf
+ 
 fi
 
 service zabbix-server start
