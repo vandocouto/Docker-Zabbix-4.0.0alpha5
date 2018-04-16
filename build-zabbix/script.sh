3caab61d198007c96ebdb3fa3647c218ac5daa55
root <root@zabbix.d2d.com.br>
2018-04-16 12:19:01 -0300

    ok

--- build-zabbix/script.sh
+++ build-zabbix/script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /usr/share/doc/zabbix-server-mysql/
-echo "CREATE DATABASE zabbix" | mysql -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
-zcat create.sql.gz | mysql zabbix -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS 
+
+
+echo "USE zabbix" |  mysql -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
+
+if [ $? -ne 0 ]; then
+	cd /usr/share/doc/zabbix-server-mysql/
+	echo "CREATE DATABASE zabbix" | mysql -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS
+	zcat create.sql.gz | mysql zabbix -u $ZB_MYSQL_USER -h $ZB_MYSQL_HOST -p$ZB_MYSQL_PASS 
+fi
+
+service zabbix-server start
+service zabbix-agent start
+/usr/sbin/apache2ctl -D FOREGROUND
